39cd9af3566fad7dbc35fa8706cba77c9b105aa9
Gustaf Lindgren <gustaf.lindgren@protonmail.com>
2024-09-04 11:32:53 +0200

    added retro styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='crt-animation'>
+      <h1>Test test</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit molestias vitae suscipit facere esse voluptas 
+        deserunt consequuntur dicta, libero sed asperiores, amet nobis voluptatem! Dolor nemo suscipit a voluptatem consectetur.</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
